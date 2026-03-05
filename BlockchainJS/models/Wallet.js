e77d6ac9ea1599c272fe7cdcd7c5c9cfcae75a58
Gabriel Nazaré <gabrielhenrique0123@hotmail.com>
2026-03-05 18:26:36 -0300

    ajeitando tudo

--- BlockchainJS/models/Wallet.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const walletSchema = new mongoose.Schema({
-    nome: { type: String, required: true },
-    chave: { type: String, required: true, unique: true }
-});
-
-module.exports = mongoose.model('Wallet', walletSchema);
